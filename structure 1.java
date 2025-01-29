dced1aa575a46bf2165cf583884fbf6c5e050994
dimijimz <dimijimz96@gmail.com>
2025-01-29 11:07:58 -0500

    Began structure code and added comments

---
+++ structure 1.java
@@ -0,0 +1,8 @@
+// this is how we can structure our java programs
+
+// java programs start with a class. every line of code that runs needs to be inside a class. (we learn morn in advanced modules) TODO: Update this comment with what a class does
+class Demo { //the class can be named anything and needs to be opened with curley brackets
+    public static void main (String[] args) {
+        System.out.println("Hi there");
+    }
+}
