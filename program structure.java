29fea7e804c1845663d6b20a53a684fe669ecc6f
dimijimz <dimijimz96@gmail.com>
2025-01-30 20:29:25 -0500

    program structure

---
+++ program structure.java
@@ -0,0 +1,10 @@
+// this is how programs are structured
+
+// this is the class. we can change the name to anything we want! (we need it to be in this for our code to run)
+class Demo{
+    public static void main(String[] args){ //this is the "entry point" (main method) for our code to run. (exact keywords covered later)
+        System.out.println("Hi java!!") // the main method can contain unlimited statements
+        System.out.println("Heres another line")
+    }
+
+}
