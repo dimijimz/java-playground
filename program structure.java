65b81f4a4c1403d7d02b96a0c5d2ab789316a62a
dimijimz <dimijimz96@gmail.com>
2025-01-30 20:38:19 -0500

    first real program

--- program structure.java
+++ program structure.java
@@ -3,8 +3,8 @@
 // this is the class. we can change the name to anything we want! (we need it to be in this for our code to run)
 class Demo{
     public static void main(String[] args){ //this is the "entry point" (main method) for our code to run. (exact keywords covered later)
-        System.out.println("Hi java!!") // the main method can contain unlimited statements
-        System.out.println("Heres another line")
+        System.out.println("Hi java!!"); // the main method can contain unlimited statements
+        System.out.println("Heres another line"); // we need to end each statement with semicolons (;)
     }
 
 }
