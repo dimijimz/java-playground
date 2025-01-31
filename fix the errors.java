8cc0668fb1310ca21e5966c0579d62452a7152c7
dimijimz <dimijimz96@gmail.com>
2025-01-30 20:41:05 -0500

    Fix error

---
+++ fix the errors.java
@@ -0,0 +1,13 @@
+//class Demo{
+    //public static void main(String[] args) {
+   //     System.out.println(Java is awesome!);
+  //  }
+//}
+
+//fixed
+
+class Demo{
+    public static void main(String[] args) {
+        System.out.println("Java is awesome!");
+    }
+}
