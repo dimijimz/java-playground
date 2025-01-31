7914061e845ddab750d174a8e0c5d07e00551a09
dimijimz <dimijimz96@gmail.com>
2025-01-31 10:00:20 -0500

    Strings - Variable structure

---
+++ variables 1.java
@@ -0,0 +1,11 @@
+//here is how strings can be tied to variables and be called using methods.
+class Demo {
+    public static void main(String[] args) {
+        //string example
+        String city; //String needs to be capital, also the second word is the variable we are calling for a string.
+        //the value of city is now New York City so when we call for the string city we will get NYC
+        city = "New York City"; // we have to still put semicolons at the end for syntax
+        //now we can print the value of city like this
+        System.out.print(city);
+    }
+}
