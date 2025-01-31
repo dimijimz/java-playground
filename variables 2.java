4d8dcc150696b46e7dd52d6b14ecda8e539b4546
dimijimz <dimijimz96@gmail.com>
2025-01-31 12:27:40 -0500

    Decimals and Numbers

---
+++ variables 2.java
@@ -0,0 +1,19 @@
+// here we see how we use the type "double" to work with decimals and then "int" to work with regular integers
+class Demo {
+    public static void main(String[] args) {
+        double weight = 12.5;
+
+        System.out.println(weight);
+    }
+}
+//output: 12.5
+
+//int
+class Demo {
+    public static void main(String[] args) {
+        int age = 29;
+
+        System.out.println(age);
+    }
+}
+//output: 29
